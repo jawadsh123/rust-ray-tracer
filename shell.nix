1e1315bf01b8edfd4019796a7bd3d00b00c4cf3a
Jawad Shaikh <shaikhjawad007@gmail.com>
2022-06-05 16:28:09 +0530

    setup nix shell

---
+++ shell.nix
@@ -0,0 +1,22 @@
+# { nixPkgs ? import <nixpkgs> { }}:
+
+let
+  rustOverlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> {
+    overlays = [ rustOverlay ];
+  };
+  rust = pkgs.rust-bin.stable.latest.default.override{
+    # for rust-analyzer
+    extensions = [ "rust-src" ];
+  };
+in
+  pkgs.mkShell {
+    name = "rust-ray-tracer";
+
+    buildInputs = with pkgs; [
+      rust
+      rust-analyzer
+    ];
+
+    RUST_BACKTRACE = 1;
+  }
